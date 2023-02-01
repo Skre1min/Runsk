6cdb3e5fba48e906bdcc76a79973732c98d2602d
Fridtjof <runarbolli123@gmail.com>
2023-02-01 20:39:50 +0100

    Updated version of nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::io;
+
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+        std::process::exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(filename: &str){
+    let path = Path::new(filename);
+    let contents = fs::read_to_string(path)
+        .expect("Error reading file");
+    run(contents);  
+    println!("{}", contents);
+
+}
+fn run_prompt() {
+    let lines = io::stdin().lines();
+    for line in lines {
+        println!("got a line: {}", line.unwrap());
+        run(line.unwrap())
+    }
 }
